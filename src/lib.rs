08c755dd591589533ae8cc3a14d9e299ae0b1f13
Osspial <osspial@gmail.com>
2017-09-07 15:41:09 -0400

    Make project a lib, not a bin

--- src/lib.rs
+++ src/lib.rs
@@ -9,48 +9,48 @@ use std::marker::PhantomData;
 
 use std::fmt::{self, Debug, Formatter};
 
-fn main() {
-    let mut tree: FlatTrie<_, i32> = FlatTrie(RawTrie::new());
-    {
-        let mut cursor = tree.cursor_mut();
-        cursor.child("a").or_insert(None).enter()
-            .child("a.a").or_insert(Some(32)).enter()
-            .child("a.a.a").or_insert(Some(48)).enter()
-                .child("a.a.a.a").or_insert(None).cont().parent().enter()
-                .child("a.a.b").or_insert(Some(83)).cont().parent().enter().parent().enter()
-            .child("b").or_insert(Some(64)).cont();
+// fn main() {
+//     let mut tree: FlatTrie<_, i32> = FlatTrie(RawTrie::new());
+//     {
+//         let mut cursor = tree.cursor_mut();
+//         cursor.child("a").or_insert(None).enter()
+//             .child("a.a").or_insert(Some(32)).enter()
+//             .child("a.a.a").or_insert(Some(48)).enter()
+//                 .child("a.a.a.a").or_insert(None).cont().parent().enter()
+//                 .child("a.a.b").or_insert(Some(83)).cont().parent().enter().parent().enter()
+//             .child("b").or_insert(Some(64)).cont();
 
-        println!("{:#?}", cursor.tree);
-        cursor.child("b").unwrap_occupied().prune();
-        println!("{:#?}", cursor.tree);
-    }
+//         println!("{:#?}", cursor.tree);
+//         cursor.child("b").unwrap_occupied().prune();
+//         println!("{:#?}", cursor.tree);
+//     }
 
-    {
-        let mut cursor = tree.cursor();
-        println!("{:?}", cursor.find_leaf_after_wrapping(83).unwrap());
-    }
+//     {
+//         let mut cursor = tree.cursor();
+//         println!("{:?}", cursor.find_leaf_after_wrapping(83).unwrap());
+//     }
 
-    let mut cursor = tree.cursor();
-    'traverse: loop {
-        let child_opt = cursor.direct_children().next().cloned();
-        match child_opt {
-            Some(child) => {cursor.child(child).unwrap_occupied().enter();},
-            None => {
-                while let Entry::Vacant(..) = cursor.sibling(1) {
-                    cursor.parent().enter();
-                    if cursor.at_root() {
-                        break 'traverse;
-                    }
-                }
-                cursor.sibling(1).unwrap_occupied().enter();
-            }
-        }
-        for _ in 0..cursor.depth() {
-            print!("    ");
-        }
-        println!("{:?} {:?}", cursor.node(), cursor.leaf());
-    }
-}
+//     let mut cursor = tree.cursor();
+//     'traverse: loop {
+//         let child_opt = cursor.direct_children().next().cloned();
+//         match child_opt {
+//             Some(child) => {cursor.child(child).unwrap_occupied().enter();},
+//             None => {
+//                 while let Entry::Vacant(..) = cursor.sibling(1) {
+//                     cursor.parent().enter();
+//                     if cursor.at_root() {
+//                         break 'traverse;
+//                     }
+//                 }
+//                 cursor.sibling(1).unwrap_occupied().enter();
+//             }
+//         }
+//         for _ in 0..cursor.depth() {
+//             print!("    ");
+//         }
+//         println!("{:?} {:?}", cursor.node(), cursor.leaf());
+//     }
+// }
 
 #[derive(Debug)]
 pub struct FlatTrie<N: Eq, L>(RawTrie<N, L>);
